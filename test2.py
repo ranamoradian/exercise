8a55b450da9e9c62695fad823a3391c686d9ae35
ranamoradian <148284212+ranamoradian@users.noreply.github.com>
2023-10-20 18:54:45 +0330

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,12 @@
+print(" to creat triangle you need to enter the three side ")
+a: int(input("enter the first side: "))
+b = int(input("enter the second side: "))
+c = int(input("enter the third side: "))
+
+if a < b+c
+   if b < a+c
+       if c < a+b
+         print("you can form a triangle ")
+
+        else:
+            print("youn can not form a triangle! ")
